d83d34e57e19dd3e32649644b57ecee756499e93
erinpisters <46272862+erinpisters@users.noreply.github.com>
2019-05-18 13:50:59 -0500

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,52 @@
+// Annotated heatmap
+var xValues = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+var yValues = [''];
+
+var zLabels = //call to the database 
+
+var zValues = //call to the database 
+
+var colorscaleValue = [
+    [0, '#3D9970'],
+    [1, '#001f3f']
+  ];
+
+var data = [{
+    x: xValues,
+    y: yValues,
+    z: zValues,
+    type: 'heatmap',
+    colorscale: colorscaleValue,
+    showscale: true
+  }];
+
+var layout = {
+    title: 'Annotated Heatmap',
+    annotations: [],
+    xaxis: {
+        ticks: '',
+        side: 'top'
+    },
+    yaxis: {
+        ticks: '',
+        ticksuffix: '',
+        width: 700,
+        height: 700,
+        autoosize: false
+    }
+
+};
+
+//drop down function to reference in html
+function drop(month){
+    var value = month.value;
+}
+
+//Create the plot 
+//my div is where the calendar goes in the html 
+
+Plotly.newPlot('myDiv',data, layout)
+
+
+
