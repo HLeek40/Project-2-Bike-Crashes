4f46675b0cc8ca388a26cb24adbfe4e03d931187
kcs342kcs <kcs342kcs@gmail.com>
2019-05-21 22:36:13 -0500

    fred

--- static/map.js
+++ static/map.js
@@ -1,4 +1,20 @@
-  // Creating map object
+var mapmargin = 50;
+$('#map').css("height", ($(window).height() - mapmargin));
+$(window).on("resize", resize);
+resize();
+function resize(){
+
+    if($(window).width()>=980){
+        $('#map').css("height", ($(window).height() - mapmargin));    
+        $('#map').css("margin-top",50);
+    }else{
+        $('#map').css("height", ($(window).height() - (mapmargin+12)));    
+        $('#map').css("margin-top",-21);
+    }
+
+}
+ 
+ // Creating map object
 var myMap = L.map("map", {
   center: [35.91, -79.05],
   zoom: 14
